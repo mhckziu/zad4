5a1ad86b89bbbb76d8b7cd07310af50c1f116266
Gracjan Leszczuch <gracek132124@gmail.com>
2026-04-09 11:24:24 +0200

    figury

---
+++ Figura.java
@@ -0,0 +1,17 @@
+public abstract class Figura {
+private String kolor;
+public Figura(String kolor) {
+this.kolor = kolor;
+}
+public abstract double obliczPole();
+
+public abstract double obliczObwod();
+
+public String getKolor() {
+return kolor;
+}
+@Override
+public String toString() {
+return "Figura [kolor=" + kolor + "]";
+}
+}
